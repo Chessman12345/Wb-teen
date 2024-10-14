2d79eb29fe1eab37caab9b61cd947662d2711326
Kirill Doe <itakademia270@gmail.com>
2024-10-14 21:45:43 +0300

    react slick

--- src/components/courses/Сourses.jsx
+++ src/components/courses/Сourses.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Html from "../../../img/html.png";
 import Ux from "../../../img/UxUl.png";
 import Figma from "../../../img/figma.png";
@@ -7,75 +10,66 @@ import Angular from "../../../img/angular.png";
 import Vue from "../../../img/vue.png";
 import Frontend from "../../../img/frontend.png";
 import TypeScript from "../../../img/typescript.png";
+import "./courses.cards.css";
 import "./courses.css";
 
 const Courses = () => {
   const data = [
     {
       id: 1,
-      text: "Професия",
+      text: "Курс",
       title: "Верстальщик",
       image: Html,
       subtitle: "3мес.",
-      class: "courses_cards-one",
+      class: "courses__slider-cards courses_cards-one",
     },
     {
       id: 2,
-      text: "Професия",
-      title: "UX/UI-дизайнер",
-      image: Ux,
-      subtitle: "11мес.",
-      class: "courses_cards-two",
-    },
-    {
-      id: 3,
-      text: "Професия",
+      text: "Курс",
       title: "Веб-дизайнер",
       image: Figma,
       subtitle: "10мес.",
-      class: "courses_cards-tree",
+      class: "courses__slider-cards courses_cards-two",
+    },
+    {
+      id: 3,
+      text: "Курс",
+      title: "React Developer",
+      image: React,
+      subtitle: "18мес.",
+      class: "courses__slider-cards courses_cards-tree",
     },
     {
       id: 4,
-      text: "Професия",
-      title: "Junior React Frontend Developer",
-      image: React,
-      subtitle: "18мес.",
-      class: "courses_cards-four",
-    },
-    {
-      id: 5,
-      text: "Професия",
-      title: "Junior Angular Frontend Developer",
-      image: Angular,
-      subtitle: "14мес.",
-      class: "courses_cards-five",
-    },
-    {
-      id: 6,
-      text: "Професия",
-      title: "Junior Vue Frontend Developer",
-      image: Vue,
-      subtitle: "14мес.",
-      class: "courses_cards-six",
-    },
-    {
-      id: 7,
-      text: "Професия",
+      text: "Курс",
       title: "Frontend Developer",
       image: Frontend,
       subtitle: "9мес.",
-      class: "courses_cards-seven",
+      class: "courses__slider-cards courses_cards-four",
     },
     {
-      id: 8,
-      text: "Професия",
-      title: "Junior TypeScript Developer",
+      id: 5,
+      text: "Курс",
+      title: "TypeScript Developer",
       image: TypeScript,
       subtitle: "10мес.",
-      class: "courses_cards-eight",
+      class: "courses__slider-cards courses_cards-five",
+    },
+    {
+      id: 6,
+      text: "Все курсы",
+      class: "courses__slider-cards courses_cards-six",
     },
   ];
+  var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
 
   return (
     <>
@@ -84,6 +78,27 @@ const Courses = () => {
           <div className="courses__container">
             <div className="courses__title">
               <h1>Курсы</h1>
+            </div>
+            <div className="courses__slider-container">
+              <Slider {...settings}>
+                {data.map((item, index) => (
+                  <div className="courses__slider-cards-inner">
+                    <div key={index} id="slider__boxes" className={item.class}>
+                      <div className="courses__slider-title-part">
+                        <div className="courses__slider-title">
+                          <h4>
+                            <a href="#">{item.text}</a>
+                          </h4>
+                          <h3>{item.title}</h3>
+                        </div>
+                      </div>
+                      <div className="courses__slider-subtitle-part">
+                        <h5>{item.subtitle}</h5>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Slider>
             </div>
           </div>
         </div>
